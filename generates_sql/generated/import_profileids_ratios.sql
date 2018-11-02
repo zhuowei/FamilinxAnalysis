acaf53e91a7b5672cfc4d92ca13362d9701674ec
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2018-11-01 18:42:21 -0700

    fix the ratio import script

--- generates_sql/generated/import_profileids_ratios.sql
+++ generates_sql/generated/import_profileids_ratios.sql
@@ -1,32 +1,32 @@
-drop table if exists profileids_bfsout;
+drop table if exists profileids_bfsout_1;
 create table profileids_bfsout_1 (
 profileid integer primary key
 );
-copy profileids_bfsout from '/tmp/output_0.01.txt';
+copy profileids_bfsout_1 from '/tmp/output_0.01.txt';
 drop table if exists profiles_bfsjoined_1;
 create table profiles_bfsjoined_1 as
 select * from profiles inner join profileids_bfsout_1 using (profileid);
-drop table if exists profileids_bfsout;
+drop table if exists profileids_bfsout_10;
 create table profileids_bfsout_10 (
 profileid integer primary key
 );
-copy profileids_bfsout from '/tmp/output_0.10.txt';
+copy profileids_bfsout_10 from '/tmp/output_0.10.txt';
 drop table if exists profiles_bfsjoined_10;
 create table profiles_bfsjoined_10 as
 select * from profiles inner join profileids_bfsout_10 using (profileid);
-drop table if exists profileids_bfsout;
+drop table if exists profileids_bfsout_15;
 create table profileids_bfsout_15 (
 profileid integer primary key
 );
-copy profileids_bfsout from '/tmp/output_0.15.txt';
+copy profileids_bfsout_15 from '/tmp/output_0.15.txt';
 drop table if exists profiles_bfsjoined_15;
 create table profiles_bfsjoined_15 as
 select * from profiles inner join profileids_bfsout_15 using (profileid);
-drop table if exists profileids_bfsout;
+drop table if exists profileids_bfsout_20;
 create table profileids_bfsout_20 (
 profileid integer primary key
 );
-copy profileids_bfsout from '/tmp/output_0.20.txt';
+copy profileids_bfsout_20 from '/tmp/output_0.20.txt';
 drop table if exists profiles_bfsjoined_20;
 create table profiles_bfsjoined_20 as
 select * from profiles inner join profileids_bfsout_20 using (profileid);
--- generates_sql/import_profileids_ratios.py
+++ generates_sql/import_profileids_ratios.py
@@ -1,10 +1,10 @@
-template = """drop table if exists profileids_bfsout;
+template = """drop table if exists profileids_bfsout_{};
 create table profileids_bfsout_{} (
 profileid integer primary key
 );
-copy profileids_bfsout from '/tmp/output_{}.txt';
+copy profileids_bfsout_{} from '/tmp/output_{}.txt';
 drop table if exists profiles_bfsjoined_{};
 create table profiles_bfsjoined_{} as
 select * from profiles inner join profileids_bfsout_{} using (profileid);"""
 for ratio in [("0.01", "1"), ("0.10", "10"), ("0.15", "15"), ("0.20", "20")]:
-	print(template.format(ratio[1], ratio[0], ratio[1], ratio[1], ratio[1]))
+	print(template.format(ratio[1], ratio[1], ratio[1], ratio[0], ratio[1], ratio[1], ratio[1]))
